35c00ab889657ab8a757716f966dae8d9c09d12c
RobertaDelrio <roberta.delrio@studio.unibo.it>
2019-02-24 23:49:40 +0100

    doxy comment

--- actions/scripts/sleep.py
+++ actions/scripts/sleep.py
@@ -21,51 +21,42 @@ from miro_constants import miro
 
 from datetime import datetime
 
+## \file sleep.py 
+## \brief The node sleep.py implements the action corresponding to the command "Sleep"
+## @n Miro closes its eyes, lights up in acquamarine, lowers its tail and inclines and moves down its head.
+
 class SleepMode():
+
     def __init__(self):
+
         ## Node rate
         self.rate = rospy.get_param('rate',200)
 
-        self.eyelid_closure = 0.0
-        self.lights_raw = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        self.tail = 0.0
-        self.ear_rotate = [0.0,0.0]
-        self.body_config = [0.0,0.0,0.0,0.0]
-        self.body_config_speed = [0.0,-1.0,-1.0,-1.0]
+        ## Publisher to the topic /miro_sleep a message of type platform_control which correspont to the sleep action
         self.pub_platform_control = rospy.Publisher('/miro_sleep',platform_control,queue_size=0)
-        
+    
+    ## Function that sets the parameters of the structure platform_control corresponding to action sleep
     def miro_sleep(self):
+
         r = rospy.Rate(self.rate)
         q = platform_control()
         
         while True:
             try:
                 q.eyelid_closure = 1.0
-                q.lights_raw = [0,255,255,0,0,0,0,0,0,0,0,0,0,0,0,0,255,255]#white color
+                q.lights_raw = [0,255,255,0,0,0,0,0,0,0,0,0,0,0,0,0,255,255] #white color
                 q.tail = -1.0
                 q.ear_rotate = [0.0,0.0]
                 q.body_config = [0.0,0.8,0.3,0.02]
                 q.body_config_speed = [0.0,-1.0,-1.0,-1.0]
                 self.pub_platform_control.publish(q)
             except KeyboardInterrupt:
-                self.eyelid_closure = 0.0
-                self.lights_raw = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-                self.tail = 0.0
-                self.ear_rotate = [0.0,0.0]
-                self.body_config = [0.0,0.0,0.0,0.0]
-                self.body_config_speed = [0.0,-1.0,-1.0,-1.0]
+
                 self.pub_platform_control.publish(q)                
                 break
             r.sleep()
 
-        #while not rospy.is_shutdown():
-            #q.eyelid_closure = 1.0
-            #q.lights_raw = [0,255,255,0,0,0,0,0,0,0,0,0,0,0,0,0,255,255]#white color
-            #q.tail = -1.0
-            #q.ear_rotate = [0.0,0.0]
-            #q.body_config = [0.0,1.2,0.6,0.7]
-            #q.body_config_speed = [0.0,-1.0,-1.0,-1.0]
-            #self.pub_platform_control.publish(q)
+       
 
 if __name__== '__main__':
     rospy.init_node('sleep', disable_signals=True)
